1d34f234a2443329549b4547fc035ea69cc907b8
VeasnaReaksa <veasnareaksa155@gmail.com>
2026-04-25 14:49:05 +0700

    Create: Love

---
+++ script.js
@@ -0,0 +1,24 @@
+const yesBtn = document.getElementById("yes");
+const noBtn = document.getElementById("no");
+
+// 👉 PUT YOUR IMAGE URL HERE
+const imageURL = "./image/rinrin.jpg"; 
+
+yesBtn.addEventListener("click", () => {
+  // Clear screen
+  document.body.innerHTML = `
+    <div style="text-align:center; padding-top:50px; background:pink; height:100vh;">
+      <h1 style="color:white;">Yas!I Love You Too Rin Rin</h1>
+      <img src="${imageURL}" style="width:300px; border-radius:20px; margin-top:20px;">
+      <h2 style="color:white;">🎉🎉🎉 Celebration 🎉🎉🎉</h2>
+    </div>
+  `;
+});
+
+// Move NO button
+noBtn.addEventListener("mouseover", () => {
+  const x = Math.floor(Math.random() * 300) - 150;
+  const y = Math.floor(Math.random() * 300) - 150;
+
+  noBtn.style.transform = `translate(${x}px, ${y}px)`;
+});
